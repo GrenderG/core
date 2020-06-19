a6fd90c21b0ed14e4ce191bfbb2155214933266a
GrenderG <grenderg@gmail.com>
2020-06-19 16:31:20 +0200

    Deleted non-blizzlike Arathi Basin Enriched Ration drops.

---
+++ sql/migrations/20200619162811_world.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20200619162811');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20200619162811');
+-- Add your query below.
+
+DELETE FROM `creature_loot_template` WHERE `item` = 20062 AND `entry` IN (11465, 11746, 14983, 15229);
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
